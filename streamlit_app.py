b4e907d7bdd611bbdaefce9bd1260d273b1bcbf4
simonarneberg0 <simon.arneberg@interworks.com>
2025-11-07 09:36:26 -0800

    Update streamlit_app.py
    shuffled around the code to get info for each fruit added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session # for SiS
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie!:cup_with_straw:")
@@ -31,6 +33,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
     #st.write(ingredients_string)
 
@@ -45,8 +49,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-# new section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
