ae6eec90f7d5f6dfd8d47122723f5143e2bb5e2f
simonarneberg0 <simon.arneberg@interworks.com>
2025-11-07 09:19:58 -0800

    Update streamlit_app.py
    corrected a typo for 'cnx = ...'

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 #session = get_active_session() #for SiS
-cnx.st.connection("snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
